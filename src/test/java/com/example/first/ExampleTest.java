0b8da56e283dd81b36c0b2efc188b20edaa0d9fe
Siraj Ahmed <sirajg75@gmail.com>
2019-06-30 14:53:54 +0530

    adding initial project structure

---
+++ src/test/java/com/example/first/ExampleTest.java
@@ -0,0 +1,17 @@
+package com.example.first;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+	
+	@Test
+	public void stringTest() {
+		
+		String s1 = "I am fine";
+		assertEquals("I am fine" , s1);
+		
+	}
+
+}
